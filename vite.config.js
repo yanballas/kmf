cd89bead33cc4bb87d1b6d651e4ac9c0bbe705dd
v.serbin <yanballas@yandex.ru>
2024-07-28 18:47:59 +0300

    feat gh-pages && readme

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
+  base: './', 
   plugins: [
     legacy({
       targets: ['defaults', 'not IE 11'],
